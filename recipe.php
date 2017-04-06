f06b7205cb95f572434084bdf96e8c77087d4ba5
Lizza Reyes <lizzareyes13@gmail.com>
2017-04-05 21:09:02 -0400

    Accessing Properties

--- recipe.php
+++ recipe.php
@@ -33,4 +33,29 @@ $bluegill = new Fish();
 
 $recipe1 = new Recipe();
 var_dump($recipe1);
+
+
   ?>
+
+<?php
+class Recipe
+{
+ public $title;
+ public $Ingredients = array();
+ public $instructions = array();
+ public $yield;
+ public $tag = array();
+ public $source = "Lizza Reyes";
+}
+
+$recipe1 = new Recipe();
+// Accessing Properties
+echo $recipe1->source;
+$recipe1->source = "Abuela Reyes";
+echo $recipe1->source;
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+echo $recipe1->source;
+echo $recipe2->source;
+ ?>
