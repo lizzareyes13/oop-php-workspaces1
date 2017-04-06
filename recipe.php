107ef4a351b6b98d0abb7c69c00c0e71bfa97b74
Lizza Reyes <lizzareyes13@gmail.com>
2017-04-05 20:30:53 -0400

    Classes and Objects

--- recipe.php
+++ recipe.php
@@ -5,4 +5,16 @@
   $recipe1 = new Recipe();
 
   var_dump($recipe1);
+
+  // create a class called Fish
+
+  class Fish
+  {
+
+  }
+//   Create an object named "bluegill" that is an instance of our Fish class.
+// Note: don't forget the keyword "new"
+
+$bluegill = new Fish();
+
  ?>
