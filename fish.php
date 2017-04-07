85b0cd6bbd02f16946ccf98637ff243e47360b3a
Lizza Reyes <lizzareyes13@gmail.com>
2017-04-06 22:45:03 -0400

    public properties / creating an object named bass

--- fish.php
+++ fish.php
@@ -1,14 +1,27 @@
 <!-- Add the following public properties to the Fish class: common_name, flavor, and record_weight. -->
 <?php
 class Fish{
-  public $common_name = array("Largemouth Bass");
-  public $flavor = array("Excellent");
-  public $record_weight = array("22 pounds 5 ounces");
+  public $common_name;
+  public $flavor;
+  public $record_weight;
 }
-// Create a Fish object named $bass with the following data:
-// name: "Largemouth Bass"
-// flavor: "Excellent"
-// record: "22 pounds 5 ounces"
 
-}
  ?>
+
+
+
+ <!-- Create a Fish object named $bass with the following data:
+ name: "Largemouth Bass"
+ flavor: "Excellent"
+ record: "22 pounds 5 ounces" -->
+ <?php
+ class Fish{
+   public $common_name;
+   public $flavor;
+   public $record_weight;
+ }
+ $bass = new Fish;
+ $bass->common_name = "Largemouth Bass";
+ $bass->flavor = "Excellent";
+ $bass->record_weight = "22 pounds 5 ounces";
+  ?>
