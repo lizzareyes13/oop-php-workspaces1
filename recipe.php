1ded210c906b3947e6c4362f4c6a8d394db7e5c1
Lizza Reyes <lizzareyes13@gmail.com>
2017-04-10 18:43:23 -0400

    access modifiers

--- recipe.php
+++ recipe.php
@@ -84,3 +84,74 @@ echo $recipe2->source;
     // end code above this line
   }
  ?>
+
+<?php
+
+  class Recipe
+  {
+    public $title;
+    public $instructions;
+    public $ingredients;
+    public $yield;
+    public $tag = array();
+    public $source = "Lizza Reyes";
+
+    public function displayRecipe()
+    {
+      return $this->title . " by " . $this->source;
+    }
+  }
+  $recipe = new Recipe();
+  $recipe->source = "Mami Feli";
+  $recipe->title = "My first recipe";
+
+  $recipe2->source = "Betty Crocker";
+  $recipe2->title = "My Second Recipe";
+
+  echo $recipe1->displayRecipe();
+  echo $recipe2->displayRecipe();
+
+
+ ?>
+
+<?php
+class Recipe
+
+{
+  // public $title;
+  private $title;
+  public $ingredients = array();
+  public $instructions = array();
+  public $yield;
+  public $tag = array();
+  public $source = "Alena Holligan";
+
+  public function setTitle($title)
+  {
+    $this->title = ucwords($title);
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  public function displayRecipe()
+  {
+    return $this->title . " by " . $this->source;
+  }
+}
+$recipe1 = new Recipe();
+$recipe1->source = "Mami Feli";
+// $recipe1->title = "My First Recipe";
+$recipe1->setTitle("my first recipe");
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+// $recipe2->title = "My Second Recipe";
+$recipe2->setTitle("my second recipe");
+
+echo $recipe1->getTitle();
+echo $recipe1->displayRecipe();
+echo $recipe2->displayRecipe();
+?>
